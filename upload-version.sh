17d5669c2e84d36d1974d6836de9f93b1262eabf
David Levanon <dvdlevanon@gmail.com>
2023-09-03 17:59:41 +0300

    add tag

--- upload-version.sh
+++ upload-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git fetch --tags
+
 last_version=$(git describe --tags `git rev-list --tags --max-count=1`)
 base=$(echo $last_version | cut -d '.' -f1-2)
 minor=$(echo $last_version | cut -d '.' -f3)
@@ -9,4 +11,9 @@ new_version="${base}.${new_minor}"
 echo "Creating new version: $new_version"
 
 git tag "$new_version"
+sed -i "s/appVersion: .*/appVersion: \"$new_version\"/" charts/Chart.yaml
+git add charts/Chart.yaml
+git commit -m "Update appVersion to $new_version"
+
+git push origin main
 git push --tags
